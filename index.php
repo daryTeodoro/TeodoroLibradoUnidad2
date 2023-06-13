5aa61123ee34a5636ddc7bc8d337e52b1007fef2
Dary Teodoro <dary_nel_3@hotmail.com>
2023-06-13 11:00:45 -0600

    Empieza el Proyecto
    Se agregan las carpetas del Proyecto y 2 de las tres interfaces de la aplicacion web. Ademas del Navbar.

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="img/spacesongLogo.png" alt="favicon">
+	<title>Space Songs</title>
+	<!--CSS Externo-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!--CSS Local-->
+    <style type="text/css">
+    	.TotalCenter{
+    		height: 100vh;
+    	}
+    </style>
+</head>
+
+<body class="Margen-0">
+	<!--Llamada al navbar-->
+	<?php include('navbar.php'); ?>
+
+	<!--Presentacion del Proyecto-->
+	<div class="ContenedorGrid">
+
+		<!-- columna Izquierda (Nombre del Sitio) -->
+		<div class="TotalCenter">
+			<h1 class="FuentePrincipal">
+				<b class="space">SPACE</b>
+				<b class="songs">SONGS</b>
+			</h1>
+		</div>
+
+		<!-- columna Derecha (Carrusel) -->
+		<div class="TotalCenter">
+			<!--Aqui va Carrusel de Imagenes con Canciones mas Escuchadas)-->
+		</div>
+
+	</div>
+
+
+	<!--Codepen.io (ejemplo de animacion: https://codepen.io/creativeocean/pen/qBbBLyB)-->
+
+
+	<!-- Canciones Jazz(Efectos de scroll) -->
+
+	<!-- Canciones Boleros(Efectos de scroll) -->
+
+	<!-- Canciones Baladas Romanticas(Efectos de scroll) -->
+
+	<!-- Canciones J-Rock(Efectos de scroll) -->
+
+	<!-- Canciones Indie(Efectos de scroll) -->
+
+</body>
+</html>
